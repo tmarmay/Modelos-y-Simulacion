6afbf178ea5445e69f05525ef33389015076fb2b
Tomas Marmay <tomasmarmay21@gmail.com>
2024-04-21 15:58:18 -0300

    resoluciones guia3

---
+++ guia3/ejercicio3.py
@@ -0,0 +1,25 @@
+import random
+
+def simulacion():
+    U = random.random()
+    w1_2 = random.random() + random.random()
+
+    if U < 1/3:
+        return w1_2
+    else:
+        w3 = random.random()
+        return w1_2 + w3
+    
+def experimento():
+    experimentos = 1_000_000
+    victoria = 0
+    puntos = [100, 1000, 10000, 100000, 1000000]
+    for i in range(experimentos):
+        X = simulacion()
+        if X <= 2:
+            victoria += 1
+        
+        if i in puntos:
+            print(f"n = {i} prob = {victoria/i}")
+
+experimento()
