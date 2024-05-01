6f5f244eca768d7d37edfd6afd336d4a25fd38a7
tmarmay <tomasmarmay21@gmail.com>
2024-05-01 20:35:23 -0300

    ejercicio6 terminado

--- guia4/ejercicio6.py
+++ guia4/ejercicio6.py
@@ -4,6 +4,13 @@ import time
 
 
 def GeneraXTI() -> int:
+    """
+    Genera un valor aleatorio de acuerdo con la probabilidad definida
+    por los rangos especificados utilizando el metodo de trasnformada a la inversa.
+
+    Returns:
+        int: El valor generado.
+    """
     U = random.random()
     if 0.45 <= U < 0.8:
         return 3
@@ -18,6 +25,19 @@ def GeneraXTI() -> int:
 
 
 def GeneraXAyR(c: float, n: int, p: float, vprob: list) -> int:
+    """
+    Genera un valor aleatorio utilizando el método de aceptación y rechazo.
+    Generado el Y con el metodo de transformada a la inversa de una binomial
+
+    Args:
+        c (float): Constante de ajuste.
+        n (int): Número de ensayos para la distribución binomial.
+        p (float): Probabilidad de éxito para la distribución binomial.
+        vprob (list): Lista de probabilidades para cada xi.
+
+    Returns:
+        int: El valor generado.
+    """
     while True:
         y = BinomialTI(n, p)
         if random.random() < vprob[y] / c:
@@ -33,6 +53,12 @@ vprob = [0.15, 0.2, 0.1, 0.35, 0.2]
 
 
 def ComparaSimulaciones(n: int):
+    """
+    Compara el tiempo de ejecución de GeneraXTI() y GeneraXAyR() para un número dado de iteraciones.
+
+    Args:
+        n (int): Número de iteraciones para comparar el rendimiento.
+    """
     comienzo = time.time()
     for _ in range(n):
         GeneraXTI()
